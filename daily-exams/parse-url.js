efbf924c56d79771de7e7f3913616452c745ab3a
stormkang <xiangxiang.kang@foxmail.com>
2020-04-19 01:04:26 +0800

    添加随笔目录

---
+++ daily-exams/parse-url.js
@@ -0,0 +1,10 @@
+/**
+ * @description 将给定的URL进行解析封装成对象并返回.
+ * @author stormkang
+ * @since Sun Apr 19, 2020 at 00:43
+ * @param {string} url 要解析的url
+ * @returns {object} 解析后封装的对象
+ */
+const parseUrl = (url) => {
+  
+}
